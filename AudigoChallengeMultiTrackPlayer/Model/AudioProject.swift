17c74aeceb74d671d69686934e6efce09e9785cb
Ken <kenmrph@gmail.com>
2020-01-15 10:29:30 -0800

    Data model for audio project

---
+++ AudigoChallengeMultiTrackPlayer/Model/AudioProject.swift
@@ -0,0 +1,13 @@
+//
+//  AudioProject.swift
+//  AudigoChallengeMultiTrackPlayer
+//
+//  Created by Ken Murphy on 1/15/20.
+//  Copyright © 2020 Ken Murphy. All rights reserved.
+//
+
+import Foundation
+
+public struct AudioProject: Codable {
+    public var tracks: [String]
+}
