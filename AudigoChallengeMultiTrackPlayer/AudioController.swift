31c7df437ed21661b3d88fc967903f91712822bb
Ken <kenmrph@gmail.com>
2020-01-15 09:41:51 -0800

    AudioController stub class

---
+++ AudigoChallengeMultiTrackPlayer/AudioController.swift
@@ -0,0 +1,13 @@
+//
+//  AudioController.swift
+//  AudigoChallengeMultiTrackPlayer
+//
+//  Created by Ken Murphy on 1/15/20.
+//  Copyright © 2020 Ken Murphy. All rights reserved.
+//
+
+import UIKit
+
+class AudioController: NSObject {
+
+}
