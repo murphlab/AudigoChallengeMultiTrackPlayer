178536d2ffd86081854abb2da671ad3d0237273d
Ken <kenmrph@gmail.com>
2020-01-15 09:03:30 -0800

    Initial Commit

---
+++ AudigoChallengeMultiTrackPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudigoChallengeMultiTrackPlayer
+//
+//  Created by Ken Murphy on 1/15/20.
+//  Copyright © 2020 Ken Murphy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AudigoChallengeMultiTrackPlayerTests/AudigoChallengeMultiTrackPlayerTests.swift
@@ -0,0 +1,34 @@
+//
+//  AudigoChallengeMultiTrackPlayerTests.swift
+//  AudigoChallengeMultiTrackPlayerTests
+//
+//  Created by Ken Murphy on 1/15/20.
+//  Copyright © 2020 Ken Murphy. All rights reserved.
+//
+
+import XCTest
+@testable import AudigoChallengeMultiTrackPlayer
+
+class AudigoChallengeMultiTrackPlayerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
