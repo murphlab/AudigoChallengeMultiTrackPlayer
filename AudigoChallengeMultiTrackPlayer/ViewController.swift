cca42c9c69bd2a8187f3aeced52da0a6f3e1647a
Ken <kenmrph@gmail.com>
2020-01-15 16:49:36 -0800

    - Wire up mute, slider actions from custom TrackCell - Define TrackCell delegate protocol - ViewController: conform to TrackCell delegate protocol

--- AudigoChallengeMultiTrackPlayer/ViewController.swift
+++ AudigoChallengeMultiTrackPlayer/ViewController.swift
@@ -61,7 +61,24 @@ extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TrackCell", for: indexPath) as! TrackCell
         cell.trackNameLabel.text = audioController.audioProject?.tracks[indexPath.row]
+        cell.delegate = self
         return cell
+    }
+}
+
+// MARK: - Track Cell Delegate
+
+extension ViewController: TrackCellDelegate {
+    func trackCell(_ trackCell: TrackCell, didTapMuteButton muteButton: UIButton) {
+        let idx = tableView.indexPath(for: trackCell)!.row
+        let name = audioController.audioProject!.tracks[idx]
+        print("MUTE BUTTON TAPPED FOR TRACK INDEX: \(idx) NAME: \(name)")
+    }
+    
+    func trackCell(_ trackCell: TrackCell, didChangeVolumeSlider: UISlider) {
+        let idx = tableView.indexPath(for: trackCell)!.row
+        let name = audioController.audioProject!.tracks[idx]
+        print("VOLUME SLIDER FOR TRACK INDEX: \(idx) NAME: \(name)")
     }
 }
 
@@ -69,4 +86,19 @@ class TrackCell: UITableViewCell {
     @IBOutlet weak var trackNameLabel: UILabel!
     @IBOutlet weak var muteButton: UIButton!
     @IBOutlet weak var volumeSlider: UISlider!
+    
+    weak var delegate: TrackCellDelegate?
+    
+    @IBAction func muteButtonTapped(_ sender: UIButton) {
+        delegate?.trackCell(self, didTapMuteButton: sender)
+    }
+    
+    @IBAction func volumeSliderChanged(_ sender: UISlider) {
+        delegate?.trackCell(self, didChangeVolumeSlider: sender)
+    }
+}
+
+protocol TrackCellDelegate: class {
+    func trackCell(_ trackCell: TrackCell, didTapMuteButton muteButton: UIButton)
+    func trackCell(_ trackCell: TrackCell, didChangeVolumeSlider: UISlider)
 }
